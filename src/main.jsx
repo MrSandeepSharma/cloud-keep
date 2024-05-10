e1a3f9008f1f720c8a251f2925e64466b101451b
Sandeep <sandipsharma2445@gmail.com>
2024-05-10 13:19:09 +0530

    Routes Setup using React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h1>Error Page</h1>,
+    children: [
+      {
+        path: "/",
+        element: <h1>Home Page</h1>
+      },
+      {
+        path: "/login",
+        element: <h1>Login Page</h1>
+      },
+      {
+        path: "/signup",
+        element: <h1>Signup Page</h1>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
