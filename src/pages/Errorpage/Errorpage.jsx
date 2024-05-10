e9e5f7b62ab6d02b59b07f66410e0bce9cc01022
Sandeep <sandipsharma2445@gmail.com>
2024-05-10 14:39:17 +0530

    Error page Added

--- src/pages/Errorpage/Errorpage.jsx
+++ src/pages/Errorpage/Errorpage.jsx
@@ -1,8 +1,21 @@
+import { Primarybtn } from "../../components/Button"
+
 import "./errorpage.css"
+
+import iconImg from "../../assets/logo-large.svg"
+import { FaChevronLeft } from "react-icons/fa";
 
 function Errorpage() {
   return (
-    <h1>Error Page</h1>
+    <div className="errorpage flex-container">
+      <img src={iconImg} alt="cloud keep" />
+      <h1 className="errorpage__title">404</h1>
+      <p className="errorpage__desc">
+        Oops! The page you're looking for isn't here. Why not head back to our 
+        homepage and explore from there?
+      </p>
+      <Primarybtn icon={<FaChevronLeft />} path="/" text="Go to Homepage" />
+    </div>
   )
 }
 
