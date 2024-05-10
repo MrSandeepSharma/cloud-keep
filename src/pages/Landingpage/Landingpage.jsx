e9e283dab48f4457db463f4cf4caab034df25c9c
Sandeep <sandipsharma2445@gmail.com>
2024-05-10 14:49:38 +0530

    Landingpage Hero Section Added

--- src/pages/Landingpage/Landingpage.jsx
+++ src/pages/Landingpage/Landingpage.jsx
@@ -1,13 +1,35 @@
 import { Footer, Header } from "../../components"
+import { Primarybtn } from "../../components/Button"
 
 import "./landingpage.css"
+
+import mobileIllustrationImg from "../../assets/mobile-illustration.png"
+import mobileIllustrationLargeImg from "../../assets/mobile-illustration-large.png"
+
+import { FaArrowRight } from "react-icons/fa6";
 
 function Landingpage() {
   return (
     <>
       <Header className="bg-clr-accent-600" btnPath="/signup" btnText="Get Started" />
-      <main id="main">
-        <h1>Landing Page</h1>
+      <main id="main" className="bg-clr-accent-600 container homepage">
+        <section className="hero flex-container">
+          <div className="hero__details">
+            <h1 className="hero__title">
+              Keep Your Files Safe, Accessible, and Organized
+            </h1>
+            <p className="hero__desc">
+              Experience seamless cloud storage solutions designed to 
+              safeguard your digital assets, ensure easy accessibility, 
+              and keep your files impeccably organized. Join us today.
+            </p>
+            <Primarybtn path="/signup" text="Try Cloud Keep For Free" icon={<FaArrowRight />} />
+          </div>
+          <picture className="hero__img">
+            <source media="(min-width: 1200px)" srcSet={mobileIllustrationLargeImg} width="526.17" height="550" />
+            <img src={mobileIllustrationImg} width="367" height="550" />
+          </picture>
+        </section>
       </main>
       <Footer />
     </>
