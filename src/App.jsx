d5dfbc154cbe533ab02bb561e155085749397118
Sandeep <sandipsharma2445@gmail.com>
2024-05-11 14:47:31 +0530

    current user check added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+
+import { Loader } from './components';
+import { login } from "./store/authSlice";
 
 import './App.css'
 
 function App() {
-  return (
-    <Outlet />
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const auth = getAuth();
+
+    const listener = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userData = {
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          uid: user.uid
+      }
+      dispatch(login(userData));
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      listener();
+    };
+
+  }, [])
+
+  return !loading ? <Outlet /> : <Loader />
+
 }
 
 export default App;
