cddc4acce4e9344fd00ff0d1d462a1a89badf3b5
Sandeep <sandipsharma2445@gmail.com>
2024-05-11 15:01:20 +0530

    some basic user detail and logout functinality added on Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,27 @@
+import { useSelector, useDispatch } from "react-redux"
+
+import { Primarybtn } from "../../components/Button"
+
+import authService from "../../firebase-local/auth"
+import { logout } from "../../store/authSlice"
+
 import "./homepage.css"
 
 function Homepage() {
+
+  const user = useSelector(state => state.auth.userData)
+  const dispatch = useDispatch()
+
+  function logoutUser() {
+    authService.logoutCurrentUser()
+    dispatch(logout())
+}
+
   return (
     <main id="main">
-      <h1>Home Page</h1>
+      <h1>{user ? user.displayName : ""}</h1>
+      <h3>{user ? user.email : ""}</h3>
+      <Primarybtn type="button" text="Logout User" onClick={logoutUser} />
     </main>
   )
 }
