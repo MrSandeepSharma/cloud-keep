6336e03a8bdf56ec9c22e81f1f8171160e2fb59e
Sandeep <sandipsharma2445@gmail.com>
2024-05-11 10:42:52 +0530

    react-toastify installed and ToastMsg component created

---
+++ src/components/ToastMsg.jsx
@@ -0,0 +1,11 @@
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+function ToastMsg() {
+  return (
+    <ToastContainer />
+  )
+}
+
+export default ToastMsg
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Logo from "./Logo";
 import { Header } from "./Header";
 import Footer from "./Footer";
 import { Input, InputPassword } from "./Input"
+import ToastMsg from "./ToastMsg";
 
-export { Logo, Header, Footer, Input, InputPassword }
+export { Logo, Header, Footer, Input, InputPassword, ToastMsg }
