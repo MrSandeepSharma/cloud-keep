4e8ab53d945aaf1e347538c302d5b4a8801de2f1
Sandeep <sandipsharma2445@gmail.com>
2024-05-11 09:49:28 +0530

    Signuppage OTP Section Added

--- src/pages/Signuppage/Signuppage.jsx
+++ src/pages/Signuppage/Signuppage.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 import { Footer, Header, Input, InputPassword } from "../../components"
 import { Primarybtn, Secondarybtn } from "../../components/Button"
 
 import "./signuppage.css"
 
-import { FaGoogle } from "react-icons/fa";
+import { FaGoogle, FaChevronLeft } from "react-icons/fa";
 import { SiGhostery } from "react-icons/si";
 
 function Signuppage() {
 
+  const [showOtp, setShowOtp] = useState(false)
+
   function sendOTP(e) {
     e.preventDefault()
+    setShowOtp(true)
     console.log("OTP Sent")
+  }
+
+  function submitOTP(e) {
+    e.preventDefault()
+    console.log("OTP Submitted")
+  }
+
+  function resendOTP() {
+    console.log("OTP Resend")
   }
 
   function loginWithGoogle() {
@@ -30,18 +44,36 @@ function Signuppage() {
           <p className="signuppage__desc">
             sign up today and take control of your files like never before!
           </p>
-          <div className="signuppage__inner">
-            <form onSubmit={sendOTP} className="signup__form">
-              <Input type="text" name="username" placeholder="Enter Your Name Here ...*" />
-              <Input type="email" name="email" placeholder="Enter Your Email Here ...*" />
-              <InputPassword name="password" placeholder="Enter Your Password Here ...*" />
-              <Primarybtn type="submit" text="Sign up" />
-            </form>
-            <div className="flex-container btn-container">
-              <Secondarybtn type="button" text="Login using Google" icon={<FaGoogle />} onClick={loginWithGoogle} />
-              <Secondarybtn type="button" text="Login as Ghost" icon={<SiGhostery />} onClick={loginAsGhost} />
-            </div>
-          </div>
+          {
+            showOtp
+            ? (
+              <div className="otppage__container">
+                <Secondarybtn icon={<FaChevronLeft />} type="btn" text="back to Signup" onClick={()=> {setShowOtp(false)}} />
+                <h2 className="otppage__title">OTP</h2>
+                <form onSubmit={submitOTP}>
+                  <Input type="number" name="otp" placeholder="Enter Your 6 digit OTP here ...*" />
+                  <div className="flex-container btn-container">
+                    <Primarybtn type="submit" text="Submit OTP" />
+                    <Secondarybtn type="button" text="Resend OTP" onClick={resendOTP} />
+                  </div>
+                </form>
+                <p className="otppage__desc">A One-Time Password (OTP) has been sent to your registered email address. Please check your inbox (and also the spam folder, just in case) for an email from us</p>
+              </div>
+            ) : (
+              <div className="signuppage__inner">
+                <form onSubmit={sendOTP} className="signup__form">
+                  <Input type="text" name="username" placeholder="Enter Your Name Here ...*" />
+                  <Input type="email" name="email" placeholder="Enter Your Email Here ...*" />
+                  <InputPassword name="password" placeholder="Enter Your Password Here ...*" />
+                  <Primarybtn type="submit" text="Sign up" />
+                </form>
+                <div className="flex-container btn-container">
+                  <Secondarybtn type="button" text="Login using Google" icon={<FaGoogle />} onClick={loginWithGoogle} />
+                  <Secondarybtn type="button" text="Login as Ghost" icon={<SiGhostery />} onClick={loginAsGhost} />
+                </div>
+              </div>
+            )
+          }
         </section>
       </main>
       <Footer />
